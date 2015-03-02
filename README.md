0fd4e09a49c7b406bb57b205399077c9539059ec
m-tsai <m-tsai@netmile.co.jp>
2015-03-02 21:17:54 +0900

    add option include_column_names

--- README.md
+++ README.md
@@ -13,7 +13,11 @@ Example:
 Use the following script in your controller
 
 ```ruby
-self.response_body = CsvExporter.export_by_line(@posts, headers)
+self.response_body = CsvExporter.export_by_line(@posts, headers) 
+#args => 
+  #data => data array of model
+  #headers => response headers
+  #options => optional
 ```
 
 CvsExporter will output all columns in the model for default.
@@ -24,13 +28,14 @@ use the following option
 self.response_body = CsvExporter.export_by_line(@posts, headers, :structure => ["id", "author.id", "author.name"])
 ```
 
-CsvExporter will look up your I18n file for column names.
+CsvExporter will look up your i18n file for column names.
 
-## options
+## Options
 
 ```ruby
-nkf #string, CsvExporter use NKF for csv file encoding, the default value is "-s"
-charset #string, default is Shift_JIS 
+:nkf #string, CsvExporter use NKF for csv file encoding, the default value is "-s"
+:charset #string, default is "Shift_JIS"
+:include_column_names #boolean, output column name in first row, default is true 
 ```
 
 ## Copyright
--- lib/csv_exporter.rb
+++ lib/csv_exporter.rb
@@ -16,22 +16,28 @@ class CsvExporter
     headers.merge!('Content-Type' => "text/csv; charset=#{options[:charset]}",'Content-Disposition' => "attachment; filename=\"#{data.model}_#{Time.now}.csv\"")
     return Rack::Chunked::Body.new(Enumerator.new do |y|
       begin
-        human_name = []
-        if options[:structure].present?
-          human_name.concat I18n.t(options[:structure], :scope => [:activerecord, :attributes, data.model.table_name.singularize])
-        else
-          data.model.column_names.each do |column_name|
-            human_name << data.model.human_attribute_name(column_name)
-          end
-        end
-        y << NKF::nkf(options[:nkf], CSV.generate_line(human_name))
+        options[:include_column_names] ||= true 
         data.find_each do |row|
+          if options[:include_column_names]
+            human_name = []
+            if options[:structure].present?
+              options[:structure].each do |s|
+                human_name << I18n.t(s, :scope => [:activerecord, :attributes, data.model.table_name.singularize], :default => s)                
+              end
+            else
+              human_name.concat I18n.t(row.attributes.keys, :scope => [:activerecord, :attributes, data.model.table_name.singularize])
+            end
+            options[:include_column_names] = false
+            y << NKF::nkf(options[:nkf], CSV.generate_line(human_name))
+          end
+          
           if options[:structure].present?
             output_row = []
             options[:structure].each do |s|
               begin
                 output_row << s.split(".").inject(row){|obj, met| obj.send(met)}
               rescue
+                output_row << ""
               end
             end
             y << NKF::nkf(options[:nkf], CSV.generate_line(output_row))
