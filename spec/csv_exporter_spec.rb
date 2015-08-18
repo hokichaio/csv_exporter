7566bf3a92ae5f4e51010d3ab1b0357fc4f1694d
m-tsai <m-tsai@netmile.co.jp>
2015-08-18 18:23:58 +0900

    add travis

---
+++ spec/csv_exporter_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+require 'csv_exporter'
+
+describe CsvExporter do
+    
+  context "a simple test" do
+    it "should pass" do
+      expect(1).to eq(1)
+    end
+  end
+    
+end
